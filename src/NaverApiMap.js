e29119bf3116a75eb23ead8543a19db419086606
gdwb3219 <gdwb3219@gmail.com>
2022-03-02 21:57:21 +0900

    설명 추가

--- src/NaverApiMap.js
+++ src/NaverApiMap.js
@@ -8,7 +8,7 @@ export const NaverAPIMap = (props) => {
         id={"map"}
         mapDivId={"react-naver-map"} // default name
         style={{
-          width: "100%", // 네이버지도 가로 길이
+          width: "100%", // 네이버지도 가로 길이 "git TEST"
           height: "85vh", // 네이버지도 세로 길이
         }}
         defaultCenter={{ lat: 37.554722, lng: 126.970833 }}
