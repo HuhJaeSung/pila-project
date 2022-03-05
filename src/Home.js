522a339c56a55d0cba001ad9996090c372014cc6
gdwb3219 <gdwb3219@gmail.com>
2022-03-05 18:16:10 +0900

    add components folder & Navbar.js

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./Nav";
 import NaverApiMap from "./NaverApiMap";
 
 function Home() {
+  const [event, setEvent] = useState();
+
+  const handleClick = (e) => {
+    console.log(e.target.value);
+    setEvent(e.target.value);
+  }
   return (
     <div className={"outline"}>
       <div className={"container"}>
-        <div className="nav"> Nav Bar </div>
+        <NavBar onClick={handleClick}></NavBar>
         <h1>Map</h1>
-        <NavBar></NavBar>
         <NaverApiMap></NaverApiMap>
         <div className={"map"}></div>
       </div>
