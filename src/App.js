d41364630917c4ec15e393382e601e8d049c8ead
gdwb3219 <gdwb3219@gmail.com>
2022-03-05 20:42:05 +0900

    Add Sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./Home";
 
 function App() {
   return (
-    <div>
+    <>
+      <Navbar />
       <Home />
-    </div>
+    </>
   );
 }
 
