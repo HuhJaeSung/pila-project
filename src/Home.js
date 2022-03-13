045546f0bdadc2c96de5f65b049c2a672ac1b807
gdwb3219 <gdwb3219@gmail.com>
2022-03-13 22:58:20 +0900

    navbar get fixed at viewport

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ function Home(props) {
   return (
     <div className={"outline"}>
       <div className={"container"}>
+        <h1>Space</h1>
         <h1>Map</h1>
         <NaverApiMap position = {props}></NaverApiMap>
         <div className={"map"}></div>
