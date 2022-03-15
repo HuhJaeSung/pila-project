5b307f0b095fe4e30c499036e50e540be609176e
gdwb3219 <gdwb3219@gmail.com>
2022-03-15 23:35:06 +0900

    Add !!! at Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <>
       <div className='footer-background'>
-        <h1>This is Footer Area</h1>
+        <h1>This is Footer Area!!!</h1>
       </div>
     </>
   )
