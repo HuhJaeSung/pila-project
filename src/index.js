1b288b36c74f28cff3a17aa992efd593a3e9270b
gdwb3219 <gdwb3219@gmail.com>
2022-03-16 19:28:39 +0900

    merge branch

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { RenderAfterNavermapsLoaded } from "react-naver-maps";
-<<<<<<< HEAD
-=======
 import { BrowserRouter } from "react-router-dom";
->>>>>>> 6f1f3d1a2ed5536667fd440060e12dce893c9c5a
 import "./style.css";
 
 ReactDOM.render(
