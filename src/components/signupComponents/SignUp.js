fb9cc1283382cb7c7cd586fd9f92a5ef88b4ed42
gdwb3219 <gdwb3219@gmail.com>
2022-09-01 23:44:21 +0900

    Modify SignUp Page with flex component

--- src/components/signupComponents/SignUp.js
+++ src/components/signupComponents/SignUp.js
@@ -1,22 +1,28 @@
-import React, { useState } from "react";
-import styles from "./SignUp.module.css";
+import React, { useState } from 'react';
+import styles from './SignUp.module.css';
 
 function SignUp() {
   // const [submit, setSubmit] = useState('');
 
   const handleSubmit = (e) => {
     // setSubmit(e.target.value)
-    console.log('Hi')
-  }
+    console.log('Hi');
+  };
   return (
     <>
       <div className={styles.container}>
         <div id={styles.SignUpContainer}>
           <form className={styles.inputForm}>
+            <label>성명</label>
+            <input placeholder="이름" />
             <label>ID</label>
             <input placeholder="아이디" />
             <label>Password</label>
             <input placeholder="비밀번호" />
+            <label>연락처</label>
+            <input placeholder="휴대폰 번호" />
+            <label>E-mail</label>
+            <input placeholder="abc@naver.com" />
             <button onClick={handleSubmit}>Submit</button>
           </form>
         </div>
