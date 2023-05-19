77bba5a5b7f354a4aa4b8b183647a9b971e0ba25
gdwb3219 <gdwb3219@gmail.com>
2023-05-20 01:05:31 +0900

    What is Children of Provider?

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { RenderAfterNavermapsLoaded } from "react-naver-maps";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./style.css";
-import CourseForm from "./components/CourseForm";
-import CenterForm from "./components/CenterForm";
-import AppStateProviders from "./providers/AppStateProviders";
-import SignUp from "./components/signupComponents/SignUp";
-import SignIn from "./components/signupComponents/SignIn";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { RenderAfterNavermapsLoaded } from 'react-naver-maps';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './style.css';
+import CourseForm from './components/CourseForm';
+import CenterForm from './components/CenterForm';
+import AppStateProviders from './providers/AppStateProviders';
+import SignUp from './components/signupComponents/SignUp';
+import SignIn from './components/signupComponents/SignIn';
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <RenderAfterNavermapsLoaded ncpClientId={"9gnpxruwbr"}>
+      <RenderAfterNavermapsLoaded ncpClientId={'9gnpxruwbr'}>
         <AppStateProviders>
+          {/* children */}
           <Routes>
-            <Route path='/' element={<App />} />
-            <Route path='form'>
+            <Route path="/" element={<App />} />
+            <Route path="form">
               <Route index element={<CenterForm />} />
-              <Route path=':courseId' element={<CourseForm />} />
+              <Route path=":courseId" element={<CourseForm />} />
             </Route>
-            <Route path='SignUp' element={<SignUp />} />
-            <Route path='SignIn' element={<SignIn />} />
+            <Route path="SignUp" element={<SignUp />} />
+            <Route path="SignIn" element={<SignIn />} />
           </Routes>
+          {/* children */}
         </AppStateProviders>
       </RenderAfterNavermapsLoaded>
     </React.StrictMode>
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
