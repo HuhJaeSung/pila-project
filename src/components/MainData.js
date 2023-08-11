39037499f4450951c88fa7ddee7c3a1ee3736fba
gdwb3219 <gdwb3219@gmail.com>
2023-08-11 18:53:27 +0900

    Add json for Django

--- src/components/MainData.js
+++ src/components/MainData.js
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import useActions from '../hooks/useActions';
-import useSiderbar from '../hooks/useSiderbar';
-import CourseDetail from './CourseDetail';
-import CourseinfoCard from './CourseinfoCard';
-import './MainData.css';
-import Footer from './Footer';
-import { AppStateContext } from '../providers/AppStateProviders';
+import React, { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import useActions from "../hooks/useActions";
+import useSiderbar from "../hooks/useSiderbar";
+import CourseDetail from "./CourseDetail";
+import CourseinfoCard from "./CourseinfoCard";
+import "./MainData.css";
+import Footer from "./Footer";
+import { AppStateContext } from "../providers/AppStateProviders";
 
 function MainData() {
   // const { key, title, location, courses } = useCenter();
@@ -29,53 +29,53 @@ function MainData() {
 
   return (
     <>
-      <nav className={sidebar ? 'centers active' : 'centers'}>
-        <div className="coursebar_container">
-          <div className="center" key={center.key}>
+      <nav className={sidebar ? "centers active" : "centers"}>
+        <div className='coursebar_container'>
+          <div className='center' key={center.key}>
             <div>
               <h2>{center.title}</h2>
-              <Link to={`form`}>
+              <Link to={`form/${center.key}/update`}>
                 <button
                   onClick={() => {
-                    setMode('UPDATE');
+                    setMode("UPDATE");
                   }}
                 >
                   센터 수정하기
                 </button>
               </Link>
               <button onClick={handleDelelte}>센터 삭제하기</button>
-              <div className="close">
+              <div className='close'>
                 <button onClick={handleClose}>❌</button>
               </div>
-              <p className="center__location">위치 : {center.location}</p>
+              <p className='center__location'>위치 : {center.location}</p>
             </div>
           </div>
-          <div className="course_list_container">
+          <div className='course_list_container'>
             {center.courses && (
-              <div className="course_container">
+              <div className='course_container'>
                 {center.courses.length === 0 ? (
                   <div>
                     <h1> 등록된 강좌가 없습니다.</h1>
                   </div>
                 ) : (
-                  <ul className="courses">
+                  <ul className='courses'>
                     {center.courses.map((course) => (
-                      <li className="classcard" key={course.id}>
+                      <li className='classcard' key={course.id}>
                         <CourseinfoCard key={course.id} course={course} />
                       </li>
                     ))}
                   </ul>
                 )}
 
-                <NavLink to={`form/${center.key}`}>
-                  <div className="AddContainer">
+                <NavLink to={`form/${center.key}/addclass`}>
+                  <div className='AddContainer'>
                     <div
-                      className="AddClass"
+                      className='AddClass'
                       onClick={() => {
-                        setMode('CREATE');
+                        setMode("CREATE");
                       }}
                     >
-                      <div className="PlusIcon"></div>
+                      <div className='PlusIcon'></div>
                     </div>
                   </div>
                 </NavLink>
